85e38839fbfe2ff6d38ca421fd384ced464f1a07
Bohdan Marchak <50302855+RiyanBliTe@users.noreply.github.com>
2022-02-04 12:27:04 +0200

    added circle sprites; added planet select animation

---
+++ Scripts/CirclePlanets.gd
@@ -0,0 +1,28 @@
+extends TextureRect
+
+export(bool) var is_selected = false
+export var related_path = ""
+
+var sprites = []
+
+func _ready():
+	initialize()
+	if is_selected:
+		show_animation()
+	else:
+		hide_animation()
+
+func show_animation():
+	for sprite in sprites:
+		sprite.visible = true
+	$AnimationPlayer.play("circle_animation")
+
+func hide_animation():
+	for sprite in sprites:
+		sprite.visible = false
+
+func initialize():
+	sprites.append(get_node("CircleRed"))
+	sprites.append(get_node("CircleGreen"))
+	sprites.append(get_node("CirclePurple"))
+	sprites.append(get_node("CircleYellow"))
--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -1,7 +1,26 @@
 extends Node2D
 
+var planets = []
+
 func _ready():
-	pass
+	initialize()
 
 func _input(event):
-	pass
+	if event.is_pressed() and event.button_index == BUTTON_LEFT:
+		for planet in planets:
+			if planet.get_rect().has_point(event.position):
+				# hide all animation
+				for planet_2 in planets:
+					planet_2.is_selected = false
+					planet_2.hide_animation()
+				# set needed animation
+				planet.is_selected = true
+				planet.show_animation()
+				$BackgroundImage.texture = load(planet.related_path)
+
+func initialize():
+	planets.append(get_node("Planet_Brown"))
+	planets.append(get_node("Planet_Earth"))
+	planets.append(get_node("Planet_Pink"))
+	planets.append(get_node("Planet_Purple"))
+	planets.append(get_node("Planet_Yellow"))
